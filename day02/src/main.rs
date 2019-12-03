aedcd76953cea9a5933e990f707e767e520382af
Joshua T. Fisher <playmer@gmail.com>
2019-12-02 17:55:00 -0800

    Day 2 Part 1 is done, part 2 is mid-writing.

--- day02/src/main.rs
+++ day02/src/main.rs
@@ -1,55 +1,99 @@
-//use std::io;
-use std::io::BufRead;
-//use std::io::{BufReader};
-//
-fn lines_from_file(filename: impl AsRef<std::path::Path>) -> Vec<String> {
-    let file = std::fs::File::open(filename).expect("no such file");
-    let buf = std::io::BufReader::new(file);
-    buf.lines()
-        .map(|l| l.expect("Could not parse line"))
-        .collect()
+use num_derive::FromPrimitive;    
+use num_traits::FromPrimitive;
+
+#[derive(FromPrimitive)]
+enum Opcode {
+    Addition = 1,
+    Multiplication = 2,
+    End = 99
 }
 
-fn calculate_fuel(module : &i32) -> i32
+fn addition(index : usize, int_codes :  & mut Vec<i32>)
 {
-    let module = *module as f32;
-    let fuel = module / 3.0;
-    (fuel.floor() as i32) - 2
+    let left_address = int_codes[index + 1] as usize;
+    let right_address = int_codes[index + 2] as usize;
+    let result_address = int_codes[index + 3] as usize;
+    
+    int_codes[result_address] = int_codes[left_address] + int_codes[right_address];
 }
 
 
-
-fn calculate_full_fuel(mass : &i32) -> i32
+fn multiplication(index : usize, int_codes :  & mut Vec<i32>)
 {
-    let mass = *mass as f32;
-    let fuel_for_mass = mass / 3.0;
-    let fuel_for_mass = (fuel_for_mass.floor() as i32) - 2;
+    let left_address = int_codes[index + 1] as usize;
+    let right_address = int_codes[index + 2] as usize;
+    let result_address = int_codes[index + 3] as usize;
 
-    if fuel_for_mass < 1
+    int_codes[result_address] = int_codes[left_address] * int_codes[right_address];
+}
+
+fn run_int_code_computer(int_codes :  & mut Vec<i32>) -> i32
+{
+    let mut index = 0;
+
+    loop
     {
-        return 0;
+        let opcode = FromPrimitive::from_i32(int_codes[index]);
+        match opcode
+        {
+            Some(Opcode::Addition) => 
+            {
+                addition(index, int_codes);
+                index += 4;
+            }
+            Some(Opcode::Multiplication) => 
+            {
+                multiplication(index, int_codes);
+                index += 4;
+            }
+            Some(Opcode::End) => break,
+            None => panic!("Unknown error!")
+        }
     }
 
-    fuel_for_mass + calculate_full_fuel(&fuel_for_mass)
+    return int_codes[0];
 }
 
 fn main() 
 {
-    let input = lines_from_file("adventInput.txt");
+    let int_codes = std::fs::read_to_string("adventInput.txt")
+        .unwrap()
+        .split(",")
+        .map(|l| l.parse::<i32>().unwrap())
+        .collect::<Vec<i32>>();
 
-    let intcodes = input.iter().map(|l| l.parse::<i32>().unwrap()).collect::<Vec<i32>>();
+    {
+        let mut int_codes = int_codes;
+        // Gotta restore the gravity assist program to the "1202 program alarm" state it had just before the last computer caught fire.
+        int_codes[1] = 12;
+        int_codes[2] = 2;
+    
+        let result = run_int_code_computer(& mut int_codes);
+    
+        println!("Part 1: Result: {}", result);
+        println!("Part 1: Full int codes: {:?}", int_codes);   
+    }
 
-    // let mut fuel_sum = 0;
+    
+    {
+        let mut int_codes_final = int_codes;
 
-    // for module in &modules
-    // {
-    //     let fuel_mass = calculate_fuel(module);
+        for noun in 0 .. 100
+        {
+            for verb in 0 .. 100
+            {
+                let mut int_codes_attempt = int_codes;
+
+                int_codes[1] = 12;
+                int_codes[2] = 2;
+            
+                let result = run_int_code_computer(& mut int_codes);
+
+                if ()
+            }
+        }
         
-    //     fuel_sum += fuel_mass;
-    // }
-
-    // dbg!(fuel_sum);
-
-    println!("Part 1: {}", modules.iter().map(calculate_fuel).sum::<i32>());
-    println!("Part 2: {}", modules.iter().map(calculate_full_fuel).sum::<i32>());
+        println!("Part 1: Result: {}", result);
+        println!("Part 1: Full int codes: {:?}", int_codes_final);   
+    }
 }
